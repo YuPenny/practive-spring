47e1d3d6de4cffc47e3892039a736eba3db9d5e7
pengjunyu <pengjunyu@365ime.com>
2018-04-02 11:07:54 +0800

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+}
